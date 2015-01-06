10068b08d9b8434ff9bb587554a3026960fb57b0
David Driscoll <david.driscoll@gmail.com>
2015-01-06 13:18:59 -0500

    Process Requets Handler concept

---
+++ src/Blacklite.Framework.Domain.Process/IProcessContextProvider.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Blacklite.Framework.Domain.Process
+{
+    public interface IProcessContextProvider
+    {
+        IProcessContext GetContextFor<T>(T instance);
+    }
+}
